0effc9b558aa370c362a1b39a1f9c2bf36db369d
nemdull <dlanx@outlook.jp>
2025-10-08 23:57:53 +0900

    fix: Card Game for Two

--- ABC088B/Main.java
+++ ABC088B/Main.java
@@ -1,7 +1,42 @@
 package ABC088B;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Input
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = scanner.nextInt();
+        }
+        scanner.close();
+
+        int[] A_sorted = A.clone();
+        Arrays.sort(A_sorted);
+
+        int[] Alice = new int[N / 2];
+        int[] Bob = new int[N / 2];
+
+        for (int i = 0; i < N; i++) {
+            if (i % 2 == 0) {
+                Alice[i] = A_sorted[i];
+            } else {
+                Bob[i] = A_sorted[i];
+            }
+        }
+
+        int Alice_sum = 0;
+        for (int i = 0; i < Alice.length; i++) {
+            Alice_sum += Alice[i];
+        }
+
+        int Bob_sum = 0;
+        for (int i = 0; i < Bob.length; i++) {
+            Bob_sum += Bob[i];
+        }
+
+        System.out.println(Alice_sum - Bob_sum);
     }
 }
